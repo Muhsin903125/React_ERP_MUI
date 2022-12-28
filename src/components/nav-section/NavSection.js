95022c135245814d3bb2fa76977362f2138129fd
MuhsinMohamed <muhsin.mohamed@waypoint-systems.com>
2022-12-28 15:19:31 +0400

    Initial

---
+++ src/components/nav-section/NavSection.js
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { NavLink as RouterLink } from 'react-router-dom';
+// @mui
+import { Box, List, Collapse, ListItemText, ListItem, ListItemButton } from '@mui/material';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+//
+import { StyledNavItem, StyledNavItemIcon } from './styles'; 
+// ----------------------------------------------------------------------
+
+NavSection.propTypes = {
+  data: PropTypes.array,
+};
+
+export default function NavSection({ data = [], ...other }) {
+  const [open, setOpen] = useState(true);
+
+  function handleClick() {
+    setOpen(!open);
+  }
+
+  return (
+    <Box {...other}>
+      <List disablePadding sx={{ p: 1 }} component="nav"> 
+        {data.map((item) => (
+item.childern!==undefined &&item.childern.length>0?
+<>
+<ListItemButton onClick={() => {handleClick() }} disableGutters >
+         
+         <StyledNavItemIcon>{item.icon && item.icon}</StyledNavItemIcon>
+
+         <ListItemText disableTypography primary={item.title} />
+         {open ? <ExpandLess /> : <ExpandMore />}
+
+         
+       
+
+     </ListItemButton>
+     <Collapse in={open} timeout="auto" unmountOnExit  style={{backgroundColor:'#fff'}}>
+           <List  disablePadding>
+             {item.childern.map((item2) => (
+               <NavItem key={item2.title} item={item2} />
+             ))}
+           </List>
+         </Collapse>
+     </>
+:
+
+          <NavItem key={item.title} item={item} />
+        ))}
+      </List>
+    </Box>
+  );
+}
+
+// ----------------------------------------------------------------------
+
+NavItem.propTypes = {
+  item: PropTypes.object,
+};
+
+function NavItem({ item }) {
+
+  const { title, path, icon, info } = item;
+ 
+    return (
+
+      <StyledNavItem
+        component={RouterLink}
+        to={path}
+        sx={{
+          '&.active': {
+            color: 'text.primary',
+            bgcolor: 'action.selected',
+            fontWeight: 'fontWeightBold',
+            boxShadow: '2px 5px 7px 0px rgb(32 101 209 / 20%)'
+          },
+        }}
+      >
+        <StyledNavItemIcon>{icon && icon}</StyledNavItemIcon>
+
+        <ListItemText disableTypography primary={title} />
+
+        {info && info}
+      </StyledNavItem>
+    );
+  }
+ 
---
+++ src/layouts/dashboard/nav/config.js
@@ -0,0 +1,60 @@
+// component
+import SvgColor from '../../../components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/dashboard/app',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'dashboard Seubb',
+    path: '',
+    icon: icon('ic_analytics'),
+    childern: [
+     
+      {
+        title: 'Test 2',
+        path: '/dashboard/products3',
+        icon: icon('ic_cart'),
+      },
+      {
+        title: 'test 3',
+        path: '/dashboard/blog3',
+        icon: icon('ic_blog'),
+      },
+    ]
+  },
+  {
+    title: 'user',
+    path: '/dashboard/user',
+    icon: icon('ic_user'),
+  },
+  {
+    title: 'product',
+    path: '/dashboard/products',
+    icon: icon('ic_cart'),
+  },
+  {
+    title: 'blog',
+    path: '/dashboard/blog',
+    icon: icon('ic_blog'),
+  },
+  {
+    title: 'login',
+    path: '/login',
+    icon: icon('ic_lock'),
+  },
+  {
+    title: 'Not found',
+    path: '/404',
+    icon: icon('ic_disabled'),
+  },
+  
+];
+
+export default navConfig;
