8000b46874ab9182a4b9854f1859d3ff54329959
MuhsinMohamed <muhsin.mohamed@waypoint-systems.com>
2023-01-12 12:37:09 +0400

    updt

--- src/pages/User/ChangePassword.js
+++ src/pages/User/ChangePassword.js
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography, Stack, TextField } from '@mui/material';
+import { Grid,Card, Container, Typography, Stack, TextField, FormControl } from '@mui/material';
 import { LoadingButton } from '@mui/lab'; 
 // components 
 
@@ -11,51 +11,67 @@ import { LoadingButton } from '@mui/lab';
 
 export default function ChangePassword() {
     const theme = useTheme();
-  
+
     return (
         <>
             <Helmet>
                 <title> Change Password </title>
             </Helmet>
 
-            <Container maxWidth="xl">
-                <Typography variant="h4" sx={{ mb: 5 }}>
-                    Change Password
-                </Typography>
+            <Container>
+                <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+                    <Typography variant="h4" gutterBottom>
+                        Change Password
+                    </Typography>
+                </Stack>
+                <Card   >
+                    <Stack m={2.5} >
+                        <Grid container md={6} spacing={2} mt={1}>
+                            <Grid item xs={12} md={12}  >
+                                <FormControl fullWidth>
+                                    <TextField name="Current password"
+                                        autoComplete="off"
+                                        // onChange={(e) => setUser(e.target.value)}
+                                        // value={user}
+                                        required
+                                        label="Current Password" />
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} md={12}  >
+                                <FormControl fullWidth>
+                                    <TextField name="newpassword"
+                                        autoComplete="off"
+                                        // onChange={(e) => setUser(e.target.value)}
+                                        // value={user}
+                                        required
+                                        label="New Password" />
 
-                <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6} md={6}>
-                        <Stack spacing={2}>
-                            <TextField name="Current password"
-                                autoComplete="off"
-                                // onChange={(e) => setUser(e.target.value)}
-                                // value={user}
-                                required
-                                label="Current Password" />
-                            <TextField name="newpassword"
-                                autoComplete="off"
-                                // onChange={(e) => setUser(e.target.value)}
-                                // value={user}
-                                required
-                                label="New Password" />
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} md={12}  >
+                                <FormControl fullWidth>
+                                    <TextField name="confirmpassword"
+                                        autoComplete="off"
+                                        // onChange={(e) => setUser(e.target.value)}
+                                        // value={user}
+                                        required
+                                        label="Confirm Password" />
+                                </FormControl>
+                            </Grid>
+                            <Grid item xs={12} md={12}  >
+                                <FormControl fullWidth>
+                                    <LoadingButton fullWidth size="large" type="submit" variant="contained" >
+                                        Update Password
+                                    </LoadingButton>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                    </Stack>
 
-                            <TextField name="confirmpassword"
-                                autoComplete="off"
-                                // onChange={(e) => setUser(e.target.value)}
-                                // value={user}
-                                required
-                                label="Confirm Password" />
-
-
-                            <LoadingButton fullWidth size="large" type="submit" variant="contained" >
-                                Update Password
-                            </LoadingButton>
-                        </Stack>
-
-                    </Grid>
-
-                </Grid>
+                </Card>
             </Container>
+
+
         </>
     );
 }
