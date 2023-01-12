1d2b0353311d7ba785fe355b2ee54bd0b6401501
MuhsinMohamed <muhsin.mohamed@waypoint-systems.com>
2023-01-12 12:30:49 +0400

    mobile maxlenth set

--- src/pages/Masters/CustomerMaster.js
+++ src/pages/Masters/CustomerMaster.js
@@ -21,11 +21,11 @@ export default function CustomerMaster() {
   const [mob, setMOB] = useState();
   const handleTrnChange = (event) => {
     if (event.target.value.length > 15) {
-        return
+      return
     }
     setTRN(event.target.value);
-  }; 
-  
+  };
+
   return (
     <>
       <Helmet>
@@ -41,14 +41,14 @@ export default function CustomerMaster() {
         <Card>
           <Stack m={2.5} >
             <Grid container spacing={2} mt={1}>
-                <Grid item xs={12} md={9}  >
-                    <FormControl fullWidth>
-                      <TextField
-                        id="cust_name"
-                        label="Customer Name"
-                      />
-                    </FormControl>
-                </Grid>
+              <Grid item xs={12} md={9}  >
+                <FormControl fullWidth>
+                  <TextField
+                    id="cust_name"
+                    label="Customer Name"
+                  />
+                </FormControl>
+              </Grid>
               <Grid item xs={12} md={3}  >
                 <FormControl fullWidth>
                   <TextField
@@ -65,48 +65,49 @@ export default function CustomerMaster() {
           </Stack>
           <Stack m={2.5}>
             <Grid container spacing={2} >
-                <Grid item  xs={6} md={6}>
-                        <FormControl fullWidth>
-                            <TextField
-                                id="cust_address"
-                                label="Address"
-                                multiline
-                                rows={4}
-                                placeholder="Address"
-                            />
-                        </FormControl>
+              <Grid item xs={6} md={6}>
+                <FormControl fullWidth>
+                  <TextField
+                    id="cust_address"
+                    label="Address"
+                    multiline
+                    rows={4}
+                    placeholder="Address"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={6} md={6}>
+                <Grid item xs={12} md={12} mb={1.5} >
+                  <FormControl fullWidth>
+                    <TextField
+                      id="cust_trn"
+                      label="TRN"
+                      type="number"
+                      value={trn}
+                      onChange={handleTrnChange}
+                    />
+                  </FormControl>
                 </Grid>
-                <Grid item xs={6} md={6}>
-                    <Grid item xs={12} md={12} mb={1.5} >
-                        <FormControl fullWidth>
-                            <TextField
-                                id="cust_trn"
-                                label="TRN"
-                                type="number"
-                                value={trn}
-                                onChange={handleTrnChange}
-                            />
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12} md={12} >
-                        <FormControl fullWidth>
-                            <TextField
-                                type="number"
-                                id="cust_Mob"
-                                label="Mobile No"
-                                value={mob}
-                            />
-                        </FormControl>
-                    </Grid>
+                <Grid item xs={12} md={12} >
+                  <FormControl fullWidth>
+                    <TextField
+                      type="tel"
+                      id="cust_mob"
+                      label="Mobile No"
+                      value={mob}
+                      inputProps={{ maxLength: 10 }}
+                    />
+                  </FormControl>
                 </Grid>
+              </Grid>
             </Grid>
           </Stack>
           <Stack direction="row" alignItems="center" justifyContent="space-between" m={2.5}>
-          <Typography />
-          <Button variant="contained" startIcon={<Iconify icon="material-symbols:person-add-rounded" />}>
-            Save Customer
-          </Button>
-        </Stack>
+            <Typography />
+            <Button variant="contained" startIcon={<Iconify icon="material-symbols:person-add-rounded" />}>
+              Save Customer
+            </Button>
+          </Stack>
         </Card>
       </Container>
 
