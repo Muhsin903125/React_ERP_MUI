4f22405c557874b527858f5c9346b6350892705a
Muhsin <64016442+Muhsin903125@users.noreply.github.com>
2024-07-26 17:19:33 +0400

    Update DateSelector.js -- date bug fixed

--- src/components/DateSelector.js
+++ src/components/DateSelector.js
@@ -1,19 +1,34 @@
-import * as React from 'react';  
+import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'; 
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
 
-export default function DateSelector({ label ,value, onChange,disableFuture,size }) {  
+export default function DateSelector({ label, value, onChange, disableFuture = true, size, required = false }) {
+const handleChange = (newValue) => {
+  if (newValue) {
+    // Ensure the date is set correctly without timezone issues
+    const correctedDate = dayjs(newValue).startOf('day'); // Set to midnight
+    onChange(correctedDate.format("YYYY-MM-DD")); // Convert to JavaScript Date object
+    console.log("corrected date:", correctedDate.format("YYYY-MM-DD")); // Log the formatted date
+  } else {
+    onChange(null);
+  }
+};
+
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
         label={label}
-        value={value}
+        value={value ? dayjs(value) : null}
         views={['year', 'month', 'day']}
         disableFuture={disableFuture}
-        onChange={onChange}
-        renderInput={(params) => <TextField  {...params} size={size} />}
+        onChange={(val) => handleChange(val)}
+        renderInput={(params) => (
+          <TextField fullWidth required={required} {...params} size={size} />
+        )}
         inputFormat="DD-MM-YYYY"
       />
     </LocalizationProvider>
