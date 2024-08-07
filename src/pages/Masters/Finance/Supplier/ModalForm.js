7dffbec8b66efe23c71b3a4abdd9d08187343b60
muhsinmohamed-waypointsystems <muhsin.mohamed@waypoint-systems.com>
2024-08-07 13:12:41 +0400

    SUPPLIER

--- src/pages/Masters/Finance/Supplier/ModalForm.js
+++ src/pages/Masters/Finance/Supplier/ModalForm.js
@@ -1,58 +1,104 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { Button, Modal, Grid, TextField, Stack, Box, Typography } from '@mui/material';
-import { Formik, Form } from 'formik';
+import { Button, Modal, Grid, TextField, Stack, Box, Typography, MenuItem, FormControl, InputLabel, Select } from '@mui/material';
+import { Formik, Form, Field } from 'formik';
 import * as yup from 'yup';
 import Iconify from '../../../../components/iconify';
 import { PostCommonSp } from '../../../../hooks/Api';
 import { useToast } from '../../../../hooks/Common';
 
-const ModalForm = ({ open, onClose, initialValues  }) => {
+const ModalForm = ({ open, onClose, initialValues }) => {
   const { showToast } = useToast();
   const [isNew, setIsNew] = useState(true);
-  useEffect(() => { 
-    if (initialValues !== null ) {
+  const [code, setCode] = useState(null);
+  const [taxTreat, setTaxTreat] = useState([]);
+  const [codeEditable, setCodeEditable] = useState(false);
+
+  useEffect(() => {
+    if (initialValues !== null) {
       setIsNew(false);
     } else {
       setIsNew(true);
+      getCode();
     }
-  }, [initialValues]);
+    getTaxTreat();
+  }, [initialValues, open]);
 
   const validationSchema = yup.object().shape({
-    docType: yup.string().required('Doc Type is required'),
-    lastno: yup.number().required('Last No is required'),
-    prefix: yup.string().required('Prefix is required'),
-    length: yup.number().required('Lenght is required'),
+    docNo: yup.string().required('Doc No is required'),
+    desc: yup.string().required('Description is required'),
+    email: yup.string().email('Invalid email format'),
+    mobile: yup.string().required('Mobile number is required'),
+    trn: yup.string().max(15, 'TRN must be at most 15 characters'),
+    address: yup.string(),
+    tax: yup.string().required('Tax treatment is required'),
   });
 
   const HandleData = async (data, type) => {
     try {
-      const { Success, Data, Message } = await PostCommonSp({
-        "key": "LAST_NO_CRUD",
-        "TYPE": type, // Pass the type as a parameter
-        "LASTNO_DOCTYPE": data.docType,
-        "LASTNO_LASTNO": data.lastno,
-        "LASTNO_PREFIX": data.prefix,
-        "LASTNO_LENGTH": data.length,
+      const { Success, Message } = await PostCommonSp({
+        key: 'SUP_CRUD',
+        TYPE: type,
+        SUP_DOCNO: data.docNo,
+        SUP_DESC: data.desc,
+        SUP_EMAIL: data.email,
+        SUP_TRN: data.trn,
+        SUP_ADDRESS: data.address,
+        SUP_MOB: data.mobile.toString(),
+        SUP_TAX_TREATMENT: data.tax || 'SR',
       });
 
       if (Success) {
         showToast(Message, 'success');
         onClose();
       } else {
-        showToast(Message, "error");
+        showToast(Message, 'error');
       }
     } catch (error) {
-      console.error("Error:", error); // More informative error handling
+      console.error('Error:', error);
     }
   };
 
+  const getCode = async () => {
+    try {
+      const { Success, Data, Message } = await PostCommonSp({
+        key: 'LAST_NO',
+        TYPE: 'SUP',
+      });
+
+      if (Success) {
+        setCode(Data?.LAST_NO);
+        setCodeEditable(Data?.IS_EDITABLE);
+      } else {
+        showToast(Message, 'error');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const getTaxTreat = async () => {
+    try {
+      const { Success, Data, Message } = await PostCommonSp({
+        key: 'LOOKUP',
+        TYPE: 'TAX_TREAT',
+      });
+
+      if (Success) {
+        setTaxTreat(Data);
+      } else {
+        showToast(Message, 'error');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
     <Modal open={open} onClose={onClose}>
       <Box
         sx={{
-          width: { xs: '90%', sm: '500px', md: "720px" },
+          width: { xs: '90%', sm: '550px', md: '720px' },
           bgcolor: 'background.paper',
           borderRadius: '8px',
           boxShadow: 24,
@@ -62,81 +108,136 @@ const ModalForm = ({ open, onClose, initialValues  }) => {
         }}
       >
         <Typography variant="h4" component="h2" sx={{ mb: 3.5, display: 'flex', justifyContent: 'space-between' }}>
-          {isNew ? "Create Supplier" : "Update Supplier"}
+          {isNew ? 'Create Supplier' : 'Update Supplier'}
         </Typography>
 
         <Formik
           initialValues={{
-            docType: initialValues?.LASTNO_DOCTYPE || '',
-            lastno: initialValues?.LASTNO_LASTNO || '',
-            prefix: initialValues?.LASTNO_PREFIX || '',
-            length: initialValues?.LASTNO_LENGTH || '',
-            // id: initialValues?.R_CODE || '',  
+            docNo: initialValues?.SUP_DOCNO || code,
+            desc: initialValues?.SUP_DESC || '',
+            email: initialValues?.SUP_EMAIL || '',
+            mobile: initialValues?.SUP_MOB || '',
+            address: initialValues?.SUP_ADDRESS || '',
+            trn: initialValues?.SUP_TRN || '',
+            tax: initialValues?.SUP_TAX_TREATMENT || '',
           }}
           validationSchema={validationSchema}
           onSubmit={(values) => {
-            if (isNew)
+            if (isNew) {
               HandleData(values, 'ADD');
-            else
+            } else {
               HandleData(values, 'UPDATE');
+            }
           }}
         >
           {({ values, errors, touched, handleChange }) => (
             <Form>
               <Grid container spacing={2}>
-
                 <Grid item xs={12} sm={6}>
                   <TextField
                     fullWidth
-                    label="Doc Type"
-                    name="docType" // Ensure this matches the validation schema
-                    value={values.docType} // Use values.name instead of values.R_NAME
-                    onChange={handleChange} // This will now work correctly
-                    error={Boolean(touched.docType && errors.docType)}
-                    helperText={touched.docType && errors.docType}
+                    label="Doc No"
+                    disabled={isNew ? !codeEditable : true}
+                    name="docNo"
+                    value={values.docNo}
+                    onChange={handleChange}
+                    error={Boolean(touched.docNo && errors.docNo)}
+                    helperText={touched.docNo && errors.docNo}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                   <TextField
                     fullWidth
-                    label="Last No"
-                    name="lastno" // Ensure this matches the validation schema
-                    value={values.lastno} // Use values.name instead of values.R_NAME
-                    onChange={handleChange} // This will now work correctly
-                    error={Boolean(touched.lastno && errors.lastno)}
-                    helperText={touched.lastno && errors.lastno}
+                    label="Description"
+                    name="desc"
+                    value={values.desc}
+                    onChange={handleChange}
+                    error={Boolean(touched.desc && errors.desc)}
+                    helperText={touched.desc && errors.desc}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                   <TextField
                     fullWidth
-                    label="Prefix"
-                    name="prefix" // Ensure this matches the validation schema
-                    value={values.prefix} // Use values.name instead of values.R_NAME
-                    onChange={handleChange} // This will now work correctly
-                    error={Boolean(touched.prefix && errors.prefix)}
-                    helperText={touched.prefix && errors.prefix}
+                    type="email"
+                    label="Email"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    error={Boolean(touched.email && errors.email)}
+                    helperText={touched.email && errors.email}
                   />
-                </Grid> <Grid item xs={12} sm={6}>
+                </Grid>
+                <Grid item xs={12} sm={6}>
                   <TextField
                     fullWidth
-                    label="Length"
-                    name="length" // Ensure this matches the validation schema
-                    value={values.length} // Use values.name instead of values.R_NAME
-                    onChange={handleChange} // This will now work correctly
-                    error={Boolean(touched.length && errors.length)}
-                    helperText={touched.length && errors.length}
+                    type="number"
+                    label="Mobile"
+                    name="mobile"
+                    value={values.mobile}
+                    onChange={handleChange}
+                    error={Boolean(touched.mobile && errors.mobile)}
+                    helperText={touched.mobile && errors.mobile}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    label="TRN"
+                    name="trn"
+                    value={values.trn}
+                    onChange={handleChange}
+                    error={Boolean(touched.trn && errors.trn)}
+                    helperText={touched.trn && errors.trn}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <FormControl fullWidth error={Boolean(touched.tax && errors.tax)}>
+                    <InputLabel>Tax treatment</InputLabel>
+                    <Field
+                      as={Select}
+                      label="Tax treatment"
+                      name="tax"
+                      value={values.tax}
+                      onChange={handleChange}
+                    >
+                      {taxTreat.map((item) => (
+                        <MenuItem key={item.LK_KEY} value={item.LK_KEY}>
+                          {item.LK_VALUE}
+                        </MenuItem>
+                      ))}
+                    </Field>
+                    {touched.tax && errors.tax && (
+                      <Typography color="error" variant="caption">
+                        {errors.tax}
+                      </Typography>
+                    )}
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} >
+                  <TextField
+                    fullWidth
+                    label="Address"
+                    name="address"
+                    value={values.address}
+                    onChange={handleChange}
+                    error={Boolean(touched.address && errors.address)}
+                    helperText={touched.address && errors.address}
                   />
                 </Grid>
                 <Grid item xs={12}>
                   <Stack direction="row" alignItems="center" justifyContent="flex-end">
-                    <Button variant="outlined" color="error" startIcon={<Iconify icon="mdi:cancel" />}
+                    <Button
+                      variant="outlined"
+                      color="error"
+                      startIcon={<Iconify icon="mdi:cancel" />}
                       sx={{ mr: 2 }}
-                      onClick={onClose}>
+                      onClick={onClose}
+                    >
                       Cancel
                     </Button>
-                    <Button type="submit" variant="contained" color={isNew ? "success" : "warning"} startIcon={<Iconify icon="basil:save-outline" />}>
-                      {isNew ? "Save" : "Update"} Supplier
+                    <Button type="submit" variant="contained" color={isNew ? 'success' : 'warning'} startIcon={<Iconify icon="basil:save-outline" />}>
+                      {isNew ? 'Save' : 'Update'} Supplier
                     </Button>
                   </Stack>
                 </Grid>
--- src/pages/Masters/Finance/Supplier/index.js
+++ src/pages/Masters/Finance/Supplier/index.js
@@ -6,97 +6,110 @@ import { Link, useNavigate } from 'react-router-dom';
 
 // @mui
 import {
-    Stack,
-    Button,
-    Typography,
-    IconButton,
-    Tooltip,
-    Box,
+  Stack,
+  Button,
+  Typography,
+  IconButton,
+  Tooltip,
+  Box,
+  Container,Card,
+  CircularProgress
 } from '@mui/material';
 
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import Iconify from '../../../../components/iconify/Iconify';
-import { AuthContext } from '../../../../App';
+import Iconify from '../../../../components/iconify/Iconify'; 
 import { deleteRole, GetRoleList, PostCommonSp, saveRole } from '../../../../hooks/Api';
 import { useToast } from '../../../../hooks/Common';
 import DataTable from '../../../../components/DataTable';
-import Confirm from '../../../../components/Confirm'; 
-import ModalForm from './ModalForm';
+import Confirm from '../../../../components/Confirm';
+import ModalForm from './ModalForm'; 
 
 export default function Supplier() {
-    const columns = [
+  const columns = [
 
-        {
-            accessorKey: 'R_CODE', //  access nested data with dot notation
-            header: 'Id',
-            // size:"300"
-        },
-        {
-            accessorKey: 'R_NAME',
-            header: 'Name',
-        },
-        {
-            header: 'Acitons',
-            Cell: ({ row }) => (
-                <div>
-                    <Tooltip title="Edit">
-                        <IconButton onClick={() => handleEdit(row.original)}>
-                            <EditIcon />
-                        </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Delete">
-                        <IconButton onClick={() => handleDelete(row.original.id)}>
-                            <DeleteIcon />
-                        </IconButton>
-                    </Tooltip>
+    {
+      accessorKey: 'SUP_DOCNO', //  access nested data with dot notation
+      header: 'Code',
+        size:"100"
+    },
+    {
+      accessorKey: 'SUP_DESC',
+      header: 'Desc',
+    }, 
+      {
+      accessorKey: 'SUP_EMAIL',
+      header: 'Email',
+    }, 
+    {
+        accessorKey: 'SUP_TRN',
+      header: 'TRN',
+    }, 
+     {
+        accessorKey: 'SUP_MOB',
+      header: 'Mobile',
+    },
+    {
+      header: 'Acitons',
+      Cell: ({ row }) => (
+        <div>
+          <Tooltip title="Edit">
+            <IconButton onClick={() => handleEdit(row.original)}>
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Delete">
+            <IconButton onClick={() => handleDelete(row.original.SUP_DOCNO)}>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
 
-                </div>
-            ),
-            //  size:200
-        },
-    ];
+        </div>
+      ),
+      //  size:200
+    },
+  ];
+ 
+  const { showToast } = useToast();
+  const [data, setData] = useState(null)
+  const [editData, setEditData] = useState(null)
+  const [loader, setLoader] = useState(true);
+  const [showModal, SetShowModal] = useState(false)
+  useEffect(() => {
 
-    const navigate = useNavigate();
-    const { setLoadingFull } = useContext(AuthContext);
-    const { showToast } = useToast();
-    const [data, setData] = useState(null)
-    const [editData, setEditData] = useState(null);
-    const [showModal, SetShowModal] = useState(false);
-    useEffect(() => {
-        fetchList();
-    }, [])
+    fetchList();
 
-    async function fetchList() {
-        setLoadingFull(true);
-        try {
-            setLoadingFull(false);
-            const { Success, Data, Message } = await PostCommonSp({
-                "key": "SUP_CRUD",
-                "TYPE": "GET_ALL",
-            })
-            if (Success) {
-                setData(Data)
-            }
-            else {
-                showToast(Message, "error");
-            }
-        }
-        finally {
-            setLoadingFull(false);
-        }
+  }, [])
+
+  async function fetchList() {
+    setLoader(true);
+    try { 
+      const { Success, Data, Message } = await PostCommonSp({
+        "key": "SUP_CRUD",
+        "TYPE": "GET_ALL",
+      })
+      if (Success) {
+        setData(Data)
+      }
+      else {
+        showToast(Message, "error");
+      }
     }
+    finally {
+      setLoader(false);
+    }
+  }
 
-   
   const handleDelete = async (id) => {
     Confirm('Are you sure to Delete?').then(async () => {
       try {
-        setLoadingFull(true);
+        setLoader(true);
         const { Success, Data, Message } = await PostCommonSp({
           "key": "SUP_CRUD",
           "TYPE": "DELETE",
-          "R_CODE": id
-        }) 
+          "SUP_DOCNO": id
+        })
+        // const { Success, Data, Message } = await deleteRole(id)
         if (Success) {
           fetchList();
           showToast(Message, 'success');
@@ -106,12 +119,10 @@ export default function Supplier() {
         }
       }
       finally {
-        setLoadingFull(false);
+        setLoader(false);
       }
     });
   }
-
-   
   function closeModal() {
     SetShowModal(false);
     setEditData(null);
@@ -127,34 +138,37 @@ export default function Supplier() {
     setEditData(null)
   }
 
-
-    return <>
-        <Helmet>
-            <title>Suppliers</title>
-        </Helmet>
-
+  return <>
+    <Helmet>
+      <title> Supplier </title>
+    </Helmet> 
+      <Card>
         <Stack m={5} >
-            <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-                <Typography variant="h4" gutterBottom>
-                Suppliers
-                </Typography>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+            <Typography variant="h4" gutterBottom>
+            Supplier
+            </Typography>
 
-                <Button variant="contained"
-                    onClick={() => SetShowModal(true)}
-                    startIcon={<Iconify icon="eva:plus-fill" />}>
-                    New
-                </Button>
-            </Stack>
+            <Button variant="contained"
+              onClick={() => handleNew()}
+              startIcon={<Iconify icon="eva:plus-fill" />}>
+              New  
+            </Button>
+          </Stack>
 
-            {/* {data && <DataTable
-                columns={columns}
-                data={data}
-                enableExport={false}
+          {
+                    (!loader && data) ? <DataTable
+                        columns={columns}
+                        data={data}
+                        // enableRowSelection 
+                        // enableGrouping
+                        enableExport={false}
 
-            />} */}
-            <ModalForm open={showModal} initialValues={null} onClose={() => SetShowModal(false)} />
+                    /> : <CircularProgress color="inherit" />
+                }
+          <ModalForm open={showModal} initialValues={editData} onClose={() => closeModal()} />
         </Stack>
-
-    </>
+        </Card> 
+  </>
 
 }
